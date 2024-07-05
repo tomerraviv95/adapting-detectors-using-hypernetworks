aebbb2a56716f6f4e3f2f453358ea7bffc762572
tomer <tomerraviv95@gmail.com>
2024-07-05 17:12:43 +0300

    removed the rnn hypernetwork and the sequential deepsic

--- python_code/detectors/__init__.py
+++ python_code/detectors/__init__.py
@@ -1,11 +1,6 @@
 from python_code.detectors.hypernetwork_deepsic.hypernetwork_deepsic_trainer import HypernetworkDeepSICTrainer
 from python_code.detectors.recurrent_deepsic.recurrent_deepsic_trainer import RecDeepSICTrainer
-from python_code.detectors.rnn_hypernetwork_deepsic.rnn_hypernetwork_deepsic_trainer import \
-    RNNHypernetworkDeepSICTrainer
-from python_code.detectors.seq_deepsic.seq_deep_sic_trainer import SeqDeepSICTrainer
 from python_code.utils.constants import DetectorType
 
-DETECTORS_TYPE_DICT = {DetectorType.seq_deepsic.name: SeqDeepSICTrainer,
-                       DetectorType.rec_deepsic.name: RecDeepSICTrainer,
-                       DetectorType.hyper_deepsic.name: HypernetworkDeepSICTrainer,
-                       DetectorType.rnn_hyper_deepsic.name: RNNHypernetworkDeepSICTrainer}
+DETECTORS_TYPE_DICT = {DetectorType.rec_deepsic.name: RecDeepSICTrainer,
+                       DetectorType.hyper_deepsic.name: HypernetworkDeepSICTrainer}
--- python_code/detectors/hypernetwork_deepsic/hypernetwork_deepsic_trainer.py
+++ python_code/detectors/hypernetwork_deepsic/hypernetwork_deepsic_trainer.py
@@ -14,6 +14,7 @@ from python_code.utils.constants import TRAINING_TYPES_DICT, TrainingType, HIDDE
 from python_code.utils.metrics import count_parameters
 
 EPOCHS = 20
+N_REPEATS = 100
 
 
 class HypernetworkDeepSICTrainer(DeepSICTrainer):
@@ -72,7 +73,7 @@ class HypernetworkDeepSICTrainer(DeepSICTrainer):
         self.optimizer = torch.optim.Adam(total_parameters, lr=self.lr)
         for epoch in range(EPOCHS):
             print(f'Epoch {epoch + 1}/{EPOCHS}')
-            curr_batch = np.random.choice(len(hs), 100)
+            curr_batch = np.random.choice(len(hs), N_REPEATS)
             total_loss = 0
             for i in curr_batch:
                 n_users = hs[i].shape[0]
